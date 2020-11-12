96b707bbdbdc611c447faf18ca5ace2d8d2127ea
IreneRuedaVega <ruedavega.irene@gmail.com>
2020-11-12 09:11:54 +0100

    Add state into constructor

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "../stylesheets/App.scss";
+import pokemons from "../data/pokemons.json";
 
-function App() {
-  return <div>Hola mundo</div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: pokemons,
+    };
+  }
+  render() {
+    console.log(pokemons);
+    return <div>Irene</div>;
+  }
 }
 
 export default App;
